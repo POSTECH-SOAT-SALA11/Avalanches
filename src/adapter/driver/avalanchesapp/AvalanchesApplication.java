2660d4ddf6a43a66d2520cf7b5594f1e35846df9
Raphael <raphael.s.teodoro@outlook.com>
2024-05-08 23:32:26 -0300

    initial commit

---
+++ src/adapter/driver/avalanchesapp/AvalanchesApplication.java
@@ -0,0 +1,14 @@
+package avalanchesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalanchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvalanchesApplication.class, args);
+    }
+
+}
+
