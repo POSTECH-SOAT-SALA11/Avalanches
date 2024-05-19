66def32870e7bdb8a6f0a1e880a12966d135c721
hennangadelha <hennanhennan@gmail.com>
2024-05-19 00:11:49 -0300

    deleçao de cliente e implementaçao de doc do controller cliente

---
+++ src/main/java/com/avalanches/adapter/driver/controller/ClienteControllerDoc.java
@@ -0,0 +1,28 @@
+package com.avalanches.adapter.driver.controller;
+
+import com.avalanches.adapter.driver.dto.ClienteRequest;
+import com.avalanches.adapter.driver.dto.ClienteResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface ClienteControllerDoc {
+
+
+     @Operation(summary = "Cadastro de cliente",
+             description = "Endpoint utilizado para realizar o cadastro de cliente.")
+     ResponseEntity<Void> create(@Valid  @RequestBody ClienteRequest cliente);
+
+     @Operation(summary = "Identifica  cliente",
+             description = "Endpoint utilizado para realizar a identificação do  cliente.")
+     @Parameter(description = "CPF do cliente", required = true, in = ParameterIn.PATH, example = "12345678900")
+     ResponseEntity<ClienteResponse> identifica(String cpf);
+
+     @Operation(summary = "Remove cadastro do cliente",
+             description = "Endpoint utilizado para realizar a remoção de cadastro do  cliente.")
+     @Parameter(description = "CPF do cliente", required = true, in = ParameterIn.PATH, example = "12345678900")
+     ResponseEntity<Void> delete(String cpf);
+}
