04a0aba4b500cbdd96eafc4570c3e6570900ca8f
hennangadelha <hennanhennan@gmail.com>
2024-05-26 23:06:50 -0300

    correçao de listagem dos pedidos

--- src/main/java/com/avalanches/adapter/driver/handler/GlobalExceptionHandler.java
+++ src/main/java/com/avalanches/adapter/driver/handler/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.avalanches.adapter.driver.handler;
 
 import com.avalanches.core.domain.ClienteNotFoundException;
+import com.avalanches.core.domain.entities.CategoriaProduto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,5 +34,20 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErroResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        String errorMessage = ex.getCause().getMessage();
+
+        if (errorMessage.contains("Cannot deserialize value of type")) {
+            String validValues = Arrays.stream(CategoriaProduto.values())
+                    .map(Enum::name)
+                    .collect(Collectors.joining(", "));
+            errorMessage = "Valor inválido para o campo categoria. Os valores aceitos são: " + validValues + ".";
+        }
+        ErroResponse erro = new ErroResponse(HttpStatus.BAD_REQUEST.value(), errorMessage, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
     }
 }
