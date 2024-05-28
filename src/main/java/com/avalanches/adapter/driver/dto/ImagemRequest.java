6a3c0e84597d93246a550d8917db4627302c04cb
Raphael <raphael.s.teodoro@outlook.com>
2024-05-27 22:12:45 -0300

    Schemas

--- src/main/java/com/avalanches/adapter/driver/dto/ImagemRequest.java
+++ src/main/java/com/avalanches/adapter/driver/dto/ImagemRequest.java
@@ -7,12 +7,16 @@ import jakarta.validation.constraints.NotNull;
 public record ImagemRequest(
         int id,
         @NotBlank(message = "Nome é um campo obrigatório.")
+        @Schema(description = "Nome da imagem", example = "XAvalanche_frente")
         String nome,
         @NotBlank(message = "Descrição é um campo obrigatório.")
+        @Schema(description = "Descrição da imagem", example = "Imagem XAvalanche")
         String descricao,
         @NotBlank(message = "TipoConteudo é um campo obrigatório.")
+        @Schema(description = "Tipo da imagem", example = "jpg")
         String tipoConteudo,
         @NotNull(message = "Tamanho é um campo obrigatório.")
+        @Schema(description = "Tamanho da imagem", example = "10")
         int tamanho,
         @NotNull(message = "Conteúdo é um campo obrigatório.")
         @Schema(description = "Conteúdo da imagem", example = "[\"1\"]")
--- src/main/java/com/avalanches/adapter/driver/dto/PedidoRequest.java
+++ src/main/java/com/avalanches/adapter/driver/dto/PedidoRequest.java
@@ -2,20 +2,42 @@ package com.avalanches.adapter.driver.dto;
 
 import com.avalanches.core.domain.entities.Cliente;
 import com.avalanches.core.domain.entities.PedidoProduto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record PedidoRequest (
-    BigDecimal valor,
 
-    LocalDateTime dataCriacao,
+        @Schema(description = "Valor do pedido", example = "48")
+        BigDecimal valor,
 
-    LocalDateTime dataFinalizacao,
+        LocalDateTime dataCriacao,
 
-    List<PedidoProduto> listaProduto,
+        LocalDateTime dataFinalizacao,
 
-    Integer IdCliente
+        @Schema(description = "Lista de Produtos", example = "[\n" +
+                "  {\n" +
+                "    \"idProduto\": 1,\n" +
+                "    \"quantidade\": 1,\n" +
+                "    \"valorUnitario\": 10.50\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"idProduto\": 2,\n" +
+                "    \"quantidade\": 2,\n" +
+                "    \"valorUnitario\": 8.75\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"idProduto\": 3,\n" +
+                "    \"quantidade\": 1,\n" +
+                "    \"valorUnitario\": 20.00\n" +
+                "  }\n" +
+                "]")
+        List<PedidoProduto> listaProduto,
+
+        @Schema(description = "Id do cliente", example = "1")
+        Integer IdCliente
 ){
 }
