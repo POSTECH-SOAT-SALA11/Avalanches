1ae506c6a7781f35f4f96fcbf07d12a45db2c3ae
hennangadelha <hennanhennan@gmail.com>
2024-07-22 22:10:39 -0300

    correcao interface

--- src/main/java/com/avalanches/frameworksanddrivers/api/PagamentoApi.java
+++ src/main/java/com/avalanches/frameworksanddrivers/api/PagamentoApi.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/avalanches/v1/pagamento")
+@Validated
 public class PagamentoApi implements PagamentoApiInterface {
 
     @Inject
